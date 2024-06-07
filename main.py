f272a711240618252558a5478058bf8906a33000
eabalestra <agusbalestra0@gmail.com>
2024-06-07 17:43:25 -0300

    add heun method, modification in plot method to show the two graphs of the different methods

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from methods.euler_method import EulerMethod
+from methods.heun import Heun
 from utils.plot import plot
 
 if __name__ == '__main__':
-    euler_method = EulerMethod(s0=763, i0=1, r0=1, h=0.05)
+    euler_method = EulerMethod(s0=763, i0=1, r0=1, h=0.07)
+    heun_method = Heun(s0=763, i0=1, r0=1, h=0.07)
 
     beta = 0.0022
     retired_rate = 0.4477
 
     t_values_np, s_result_np, i_result_np, r_result_np = euler_method.run(20, beta, retired_rate)
+    t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun = heun_method.run(20, beta, retired_rate)
 
-    plot(t_values_np, s_result_np, i_result_np, r_result_np)
+    plot(t_values_np, s_result_np, i_result_np, r_result_np, t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun)
--- utils/plot.py
+++ utils/plot.py
@@ -1,9 +1,12 @@
 import gnuplotlib as gp
 
 
-def plot(t_values_np, s_result_np, i_result_np, r_result_np):
+def plot(t_values_np, s_result_np, i_result_np, r_result_np, t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun):
     gp.plot(
-        (t_values_np, s_result_np, {'legend': 'Susceptibles', 'with': 'lines lc rgb "green"'}),
-        (t_values_np, i_result_np, {'legend': 'Infectados', 'with': 'lines lc rgb "blue"'}),
-        (t_values_np, r_result_np, {'legend': 'Recuperados', 'with': 'lines lc rgb "red"'})
+        (t_values_np, s_result_np, {'legend': 'Susceptibles Euler', 'with': 'lines lc rgb "green"'}),
+        (t_values_np, i_result_np, {'legend': 'Infectados Euler', 'with': 'lines lc rgb "blue"'}),
+        (t_values_np, r_result_np, {'legend': 'Recuperados Euler', 'with': 'lines lc rgb "red"'}),
+        (t_values_np_heun, s_result_np_heun, {'legend': 'Susceptibles Heun', 'with': 'lines lc rgb "light-green"'}),
+        (t_values_np_heun, i_result_np_heun, {'legend': 'Infectados Heun', 'with': 'lines lc rgb "light-blue"'}),
+        (t_values_np_heun, r_result_np_heun, {'legend': 'Recuperados Heun', 'with': 'lines lc rgb "light-red"'})
     )
