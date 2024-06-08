b1125710a5481d7a454aea00c5af9761043ac9d1
eabalestra <agusbalestra0@gmail.com>
2024-06-08 09:37:07 -0300

    comments for run method

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from methods.heun import Heun
 from utils.plot import plot
 
 if __name__ == '__main__':
-    euler_method = Euler(s0=763, i0=1, r0=1, h=0.05)
-    heun_method = Heun(s0=763, i0=1, r0=1, h=0.05)
+    s0 = 763
+    i0 = 1
+    r0 = 0
+    h = 0.05
+    days = 20
+
+    euler_method = Euler(s0, i0, r0, h)
+    heun_method = Heun(s0, i0, r0, h)
 
     beta = 0.0022
     retired_rate = 0.4477
 
-    t_values_np, s_result_np, i_result_np, r_result_np = euler_method.run(20, beta, retired_rate)
-    t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun = heun_method.run(20, beta, retired_rate)
+    t_values_np, s_result_np, i_result_np, r_result_np = euler_method.run(days, beta, retired_rate)
+    t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun = heun_method.run(days, beta, retired_rate)
 
     plot(t_values_np, s_result_np, i_result_np, r_result_np, t_values_np_heun, s_result_np_heun, i_result_np_heun, r_result_np_heun)
--- methods/method.py
+++ methods/method.py
@@ -29,17 +29,25 @@ class Method(ABC):
 
     def run(self, days, beta, retired_rate):
         """
-        Run the SIR model simulation using the Heun method.
+        Runs the simulation of the SIR model using the specified method.
 
-        :param days: The number of days to run the simulation.
-        :param beta: The infection rate parameter in the SIR model.
-        :param retired_rate: The recovery rate parameter in the SIR model.
-        :return: Four numpy arrays representing the time points and the number
-        of susceptible, infected, and recovered individuals at each time point.
+        :param days: Total number of days for the simulation.
+        :param beta: Per-capita transmission rate of the disease.
+        :param retired_rate: Removal rate (rate at which infected individuals recover or die).
+        :return: Tuples of numpy arrays containing the values of time, susceptible, infected, and removed individuals.
         """
+
+        # Generate an array of time values from 0 to 'days' with a step size of 'h'
         t_values_np = np.arange(0, days, self.h)
+
+        # Calculate the values of susceptibles S(t) for each value of t in t_values_np
         s_result_np = np.array([self.s(t, beta, retired_rate) for t in t_values_np])
+
+        # Calculate the values of infected I(t) for each value of t in t_values_np
         i_result_np = np.array([self.i(t, beta, retired_rate) for t in t_values_np])
+
+        # Calculate the values of removed R(t) for each value of t in t_values_np
         r_result_np = np.array([self.r(t, beta, retired_rate) for t in t_values_np])
 
+        # Return a tuple with the arrays of time values, susceptibles, infected, and removed individuals
         return t_values_np, s_result_np, i_result_np, r_result_np
